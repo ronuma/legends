da86e9e9185986bee2495ff5d1d0a1329c612bc9
Pablo Banzo <pridapablo@gmail.com>
2023-04-17 13:31:22 -0600

    init api

---
+++ Web/Back/api/db.js
@@ -0,0 +1,64 @@
+"use strict";
+
+import express from "express";
+import mysql from "mysql2/promise";
+
+import fs from "fs";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use("/css", express.static("./css"));
+app.use("/js", express.static("./js"));
+
+app.get("/", (req, res) => {
+  fs.readFile("./public/html/mysqlUseCases.html", "utf8", (err, html) => {
+    if (err) {
+      res.status(500).send("There was an error: " + err);
+      return;
+    }
+
+    console.log("Sending page...");
+    res.send(html);
+    console.log("Page sent!");
+  });
+});
+
+app.get("/api/hello", (req, res) => {
+  if (req.query.hasOwnProperty("name") && req.query.hasOwnProperty("surname"))
+    res.send(`Hello ${req.query.name} ${req.query.surname}`);
+  else res.send("Hello!");
+});
+
+app.get("/api/users", async (req, res) => {
+  let connection = null;
+
+  try {
+    connection = await mysql.createConnection({
+      host: "localhost",
+      user: "userdb",
+      password: "userdb",
+      database: "api_game_db",
+    });
+
+    console.log("Connection stablished!");
+
+    const [rows, fields] = await connection.execute("select * from users");
+
+    res.json(rows);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+    console.log(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed succesfully!");
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ Web/Back/public/js/mysqlUseCases.js
@@ -0,0 +1,134 @@
+function main()
+{
+    document.getElementById('formSelectUser').onsubmit = async (e) =>
+    {
+        e.preventDefault()
+
+        const data = new FormData(formSelectUser)
+        const dataObj = Object.fromEntries(data.entries())
+
+        let response = await fetch(`http://127.0.0.1:5000/api/users/${dataObj['userID']}`,{
+            method: 'GET'
+        })
+        
+        if(response.ok)
+        {
+            let results = await response.json()
+        
+            if(results.length > 0)
+            {
+                const headers = Object.keys(results[0])
+                const values = Object.values(results)
+    
+                let table = document.createElement("table")
+    
+                let tr = table.insertRow(-1)                  
+    
+                for(const header of headers)
+                {
+                    let th = document.createElement("th")     
+                    th.innerHTML = header
+                    tr.appendChild(th)
+                }
+    
+                for(const row of values)
+                {
+                    let tr = table.insertRow(-1)
+    
+                    for(const key of Object.keys(row))
+                    {
+                        let tabCell = tr.insertCell(-1)
+                        tabCell.innerHTML = row[key]
+                    }
+                }
+    
+                const container = document.getElementById('getResultsID')
+                container.innerHTML = ''
+                container.appendChild(table)
+            }
+            else
+            {
+                const container = document.getElementById('getResultsID')
+                container.innerHTML = 'No results to show.'
+            }
+        }
+        else{
+            getResults.innerHTML = response.status
+        }
+    }
+
+    document.getElementById('formInsert').onsubmit = async(e)=>
+    {
+        e.preventDefault()
+
+        const data = new FormData(formInsert)
+        const dataObj = Object.fromEntries(data.entries())
+
+        let response = await fetch('http://127.0.0.1:5000/api/users',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(dataObj)
+        })
+        
+        if(response.ok)
+        {
+            let results = await response.json()
+        
+            console.log(results)
+            postResults.innerHTML = results.message
+        }
+        else{
+            postResults.innerHTML = response.status
+        }
+    }
+
+    document.getElementById('formUpdate').onsubmit = async(e)=>
+    {
+        e.preventDefault()
+
+        const data = new FormData(formUpdate)
+        const dataObj = Object.fromEntries(data.entries())
+
+        let response = await fetch('http://127.0.0.1:5000/api/users',{
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(dataObj)
+        })
+        
+        if(response.ok)
+        {
+            let results = await response.json()
+        
+            console.log(results)
+            putResults.innerHTML = results.message
+        }
+        else{
+            putResults.innerHTML = response.status
+        }
+    }
+
+    document.getElementById('formDelete').onsubmit = async(e)=>
+    {
+        e.preventDefault()
+
+        const data = new FormData(formDelete)
+        const dataObj = Object.fromEntries(data.entries())
+
+        let response = await fetch(`http://127.0.0.1:5000/api/users/${dataObj['userID']}`,{
+            method: 'DELETE'
+        })
+        
+        if(response.ok)
+        {
+            let results = await response.json()
+        
+            deleteResults.innerHTML = results.message
+        }
+        else
+        {
+            deleteResults.innerHTML = `Error!\nStatus: ${response.status} Message: ${results.message}`
+        }
+    }
+}
+
+main()
