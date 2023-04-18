c0622df3ad6db7c649637ef6dfbbc1c1d18ac076
Gabriel Rodriguez <gabordlr@gmail.com>
2023-04-17 22:03:18 -0600

    More Comments

--- Videogame/QuetzalUnity/Assets/Scripts/MapGenerator.cs
+++ Videogame/QuetzalUnity/Assets/Scripts/MapGenerator.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public GameObject[] maps;
+// An array of map prefabs to choose from
+public GameObject[] maps;
+// Create the map on start
+public void Start()
+{
+    createMap();
+}
 
-    public void Start()
-    {
-        createMap();
-    }
-
-    public void createMap()
-    {
-        int rand = Random.Range(0, maps.Length);
-        Instantiate(maps[rand], transform.position, Quaternion.identity);
-    }
-   
+// Create a new map by instantiating a random map prefab from the maps array
+public void createMap()
+{
+    int rand = Random.Range(0, maps.Length);
+    Instantiate(maps[rand], transform.position, Quaternion.identity);
+}  
 }
