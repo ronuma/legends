f78a331653b0431ce0992aa012517481dd347357
Pablo Banzo <pridapablo@gmail.com>
2023-04-19 14:01:41 -0600

    add enemies endpoint

--- Web/back/quetzal-api/helpers/characters.js
+++ Web/back/quetzal-api/helpers/characters.js
@@ -5,3 +5,9 @@ export async function getNPCs() {
    const [results, fields] = await db.execute("SELECT * FROM quetzal.NPCs");
    return results;
 }
+
+export async function getEnemies() {
+   const db = await connectToDB();
+   const [results, fields] = await db.execute("SELECT * FROM quetzal.Enemies");
+   return results;
+}
--- Web/back/quetzal-api/routes/characters.js
+++ Web/back/quetzal-api/routes/characters.js
@@ -1,6 +1,6 @@
 import {Router} from "express";
 
-import {getNPCs} from "../helpers/characters.js";
+import {getNPCs, getEnemies} from "../helpers/characters.js";
 
 const router = Router();
 
@@ -18,6 +18,25 @@ router.get("/NPCs", async (req, res) => {
       console.log("GET npcs error: ", error);
       res.status(500).json({
          msg: "GET npcs error",
+         error,
+      });
+   }
+});
+
+router.get("/enemies", async (req, res) => {
+   try {
+      const data = await getEnemies();
+      if (!data) {
+         res.status(404).json({
+            msg: "No enemies found",
+         });
+         return;
+      }
+      res.status(200).json(data);
+   } catch (error) {
+      console.log("GET enemies error: ", error);
+      res.status(500).json({
+         msg: "GET enemies error",
          error,
       });
    }
