2a191c39552745751fafbb2dd0d1ecf84455f880
Pablo Banzo <pridapablo@gmail.com>
2023-04-19 12:13:15 -0600

    add users base api

--- Web/Back/api/db.js
+++ Web/Back/api/db.js
@@ -2,53 +2,141 @@
 
 import express from "express";
 import mysql from "mysql2/promise";
-
 import fs from "fs";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  fs.readFile("./public/html/mysqlUseCases.html", "utf8", (err, html) => {
-    if (err) {
-      res.status(500).send("There was an error: " + err);
-      return;
-    }
+async function connectToDB() {
+  return await mysql.createConnection({
+    host: "localhost",
+    user: "userdb",
+    password: "userdb",
+    database: "api_game_db",
+  });
+}
 
-    console.log("Sending page...");
-    res.send(html);
-    console.log("Page sent!");
+app.get("/", (request, response) => {
+  fs.readFile("./public/html/mysqlUseCases.html", "utf8", (err, html) => {
+    if (err) response.status(500).send("There was an error: " + err);
+    console.log("Loading page...");
+    response.send(html);
   });
 });
 
-app.get("/api/hello", (req, res) => {
-  if (req.query.hasOwnProperty("name") && req.query.hasOwnProperty("surname"))
-    res.send(`Hello ${req.query.name} ${req.query.surname}`);
-  else res.send("Hello!");
-});
-
-app.get("/api/users", async (req, res) => {
+app.get("/api/users", async (request, response) => {
   let connection = null;
 
   try {
-    connection = await mysql.createConnection({
-      host: "localhost",
-      user: "userdb",
-      password: "userdb",
-      database: "api_game_db",
-    });
+    connection = await connectToDB();
+    const [results, fields] = await connection.execute("select * from users");
 
-    console.log("Connection stablished!");
-
-    const [rows, fields] = await connection.execute("select * from users");
-
-    res.json(rows);
+    console.log("QWERTY");
+    response.json(results);
   } catch (error) {
-    res.status(500);
-    res.send(error);
+    response.status(500);
+    response.json(error);
+    console.log(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed succesfully!");
+    }
+  }
+});
+
+app.get("/api/users/:id", async (request, response) => {
+  let connection = null;
+
+  try {
+    connection = await connectToDB();
+
+    const [results, fields] = await connection.query(
+      "select * from users where id_users= ?",
+      [request.params.id]
+    );
+
+    console.log("ADASDA");
+    response.json(results);
+  } catch (error) {
+    response.status(500);
+    response.json(error);
+    console.log(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed succesfully!");
+    }
+  }
+});
+
+app.post("/api/users", async (request, response) => {
+  let connection = null;
+
+  try {
+    connection = await connectToDB();
+
+    const [results, fields] = await connection.query(
+      "insert into users set ?",
+      request.body
+    );
+
+    response.json({ message: "Data inserted correctly." });
+  } catch (error) {
+    response.status(500);
+    response.json(error);
+    console.log(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed succesfully!");
+    }
+  }
+});
+
+app.put("/api/users", async (request, response) => {
+  let connection = null;
+
+  try {
+    connection = await connectToDB();
+
+    const [results, fields] = await connection.query(
+      "update users set name = ?, surname = ? where id_users= ?",
+      [request.body["name"], request.body["surname"], request.body["userID"]]
+    );
+
+    response.json({ message: "Data updated correctly." });
+  } catch (error) {
+    response.status(500);
+    response.json(error);
+    console.log(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed succesfully!");
+    }
+  }
+});
+
+app.delete("/api/users/:id", async (request, response) => {
+  let connection = null;
+
+  try {
+    connection = await connectToDB();
+
+    const [results, fields] = await connection.query(
+      "delete from users where id_users= ?",
+      [request.params.id]
+    );
+
+    response.json({ message: "Data deleted correctly." });
+  } catch (error) {
+    response.status(500);
+    response.json(error);
     console.log(error);
   } finally {
     if (connection !== null) {
@@ -59,5 +147,5 @@ app.get("/api/users", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
