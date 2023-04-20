1de2e9446849cb291016d52510c495375eb479f3
Pablo Banzo <pridapablo@gmail.com>
2023-04-20 13:49:59 -0600

    add individual item id

--- Web/back/quetzal-api/helpers/items.js
+++ Web/back/quetzal-api/helpers/items.js
@@ -5,3 +5,12 @@ export async function getItems() {
    const [results, fields] = await db.execute("SELECT * FROM quetzal.Items");
    return results;
 }
+
+export async function getItem(id) {
+   const db = await connectToDB();
+   const [results, fields] = await db.execute(
+      "SELECT * FROM quetzal.Items WHERE item_id = ?",
+      [id]
+   );
+   return results;
+}
