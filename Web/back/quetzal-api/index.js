0332c026c68448f9306327c5524b1eb7f709b2fb
Pablo Banzo <pridapablo@gmail.com>
2023-04-24 12:46:31 -0600

    add cors to backend server

--- Web/back/quetzal-api/index.js
+++ Web/back/quetzal-api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import {
    itemsRoute,
    charactersRoute,
@@ -11,6 +12,7 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(cors);
 
 // Helper function to connect to the database
 async function connectToDB() {
