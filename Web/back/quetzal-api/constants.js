63e0ad570a98bb18f0d3573535b0623545895abe
ronuma <ronum.00@gmail.com>
2023-04-25 14:55:06 -0600

    configured constants file to connect to remote DB

---
+++ Web/back/quetzal-api/constants.js
@@ -0,0 +1,16 @@
+export const DEV = {
+   host: "localhost",
+   user: "userdb",
+   password: "userdb",
+   database: "quetzal",
+};
+
+export const PROD = {
+   host: "quet.cby19a4j2vf8.us-east-1.rds.amazonaws.com",
+   user: "admin-quetzalcoatl",
+   password: "X9#sT@4fW^qL",
+   database: "quetzal",
+};
+
+export const ENV = PROD;
+export const PORT = 8000;
