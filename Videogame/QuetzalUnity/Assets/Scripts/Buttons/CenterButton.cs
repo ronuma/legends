6b4825b97cfedd4422f8365fbb47d7b7bb8011c1
Gabriel Rodriguez <gabordlr@gmail.com>
2023-05-02 19:22:50 -0600

    ItemsBarrier

--- Videogame/QuetzalUnity/Assets/Scripts/Buttons/CenterButton.cs
+++ Videogame/QuetzalUnity/Assets/Scripts/Buttons/CenterButton.cs
@@ -28,23 +28,11 @@ public class CenterButton : MonoBehaviour
 
     private void applyItem()
     {
-        player.GetComponent<PlayerStats>().playerHealth += item.GetComponent<ItemsBuff>().itemHealth;
-        player.GetComponent<PlayerStats>().playerSpeed += item.GetComponent<ItemsBuff>().itemSpeed;
-        player.GetComponent<PlayerStats>().playerDefense += item.GetComponent<ItemsBuff>().itemDefense;
-        player.GetComponent<PlayerStats>().playerDamage += item.GetComponent<ItemsBuff>().itemDamage;
-        player.GetComponent<PlayerStats>().playerMana += item.GetComponent<ItemsBuff>().itemMana;
-
-        var result = player.GetComponent<PlayerStats>().playerHealth > 0 ? player.GetComponent<PlayerStats>().playerHealth : 1;
-        var result2 = player.GetComponent<PlayerStats>().playerMana > 0 ? player.GetComponent<PlayerStats>().playerMana : 1;
-        var result3 = player.GetComponent<PlayerStats>().playerDamage > 0 ? player.GetComponent<PlayerStats>().playerDamage : 1;
-        var result4 = player.GetComponent<PlayerStats>().playerSpeed > 0 ? player.GetComponent<PlayerStats>().playerSpeed : 1;
-        var result5 = player.GetComponent<PlayerStats>().playerDefense > 0 ? player.GetComponent<PlayerStats>().playerDefense : 1;
-
-        player.GetComponent<PlayerStats>().playerHealth = result;
-        player.GetComponent<PlayerStats>().playerMana = result2;
-        player.GetComponent<PlayerStats>().playerDamage = result3;
-        player.GetComponent<PlayerStats>().playerSpeed = result4;
-        player.GetComponent<PlayerStats>().playerDefense = result5;
+        player.GetComponent<PlayerStats>().playerHealth = Mathf.Clamp(player.GetComponent<PlayerStats>().playerHealth + item.GetComponent<ItemsBuff>().itemHealth, 1, 999);
+        player.GetComponent<PlayerStats>().playerMana = Mathf.Clamp(player.GetComponent<PlayerStats>().playerMana + item.GetComponent<ItemsBuff>().itemMana, 1, 299);
+        player.GetComponent<PlayerStats>().playerDamage = Mathf.Clamp(player.GetComponent<PlayerStats>().playerDamage + item.GetComponent<ItemsBuff>().itemDamage, 1, 124);
+        player.GetComponent<PlayerStats>().playerSpeed = Mathf.Clamp(player.GetComponent<PlayerStats>().playerSpeed + item.GetComponent<ItemsBuff>().itemSpeed, 1, 7.9f);
+        player.GetComponent<PlayerStats>().playerDefense = Mathf.Clamp(player.GetComponent<PlayerStats>().playerDefense + item.GetComponent<ItemsBuff>().itemDefense, 1, 249);
 
         player.GetComponent<PlayerHealth>().ResetHealth();
 
