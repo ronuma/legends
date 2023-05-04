28b2396f3ee77f8686cf973f82e5c26cdd2eacc9
AleAro21 <A01782691@tec.mx>
2023-05-03 22:46:26 -0600

    first comments

--- Videogame/QuetzalUnity/Assets/Scripts/Character Menu/CanvasSession.cs
+++ Videogame/QuetzalUnity/Assets/Scripts/Character Menu/CanvasSession.cs
@@ -49,47 +49,57 @@ public class CanvasSession : MonoBehaviour
         rightBtn.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "";
         middleBtn.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "";
 
-
+        // Call processUserData() coroutine
         StartCoroutine(processUserData());
     }
 
+    // Coroutine that processes user data
     IEnumerator processUserData()
     {
+        // Wait for 2 seconds
         yield return new WaitForSeconds(2f);
 
+        // Get user's slots data
         slots = new Slot[3] { getUser.GetComponent<getUser>().uniqueUser.slot_1, getUser.GetComponent<getUser>().uniqueUser.slot_2, getUser.GetComponent<getUser>().uniqueUser.slot_3 };
 
+        // Update the character slots on the canvas
         UpdateCharacter(slots[0], characterSprite1, characterName1, leftBtn);
         UpdateCharacter(slots[1], characterSprite2, characterName2, middleBtn);
         UpdateCharacter(slots[2], characterSprite3, characterName3, rightBtn);
     }
 
+    // Method to update the character on the canvas
     private void UpdateCharacter(Slot index, Image characterSpriteStatic, TMPro.TextMeshProUGUI characterNameStatic, Button btn)
     {
+        // Get the player ID
         int playerId = index.hero_id > 0 ? index.hero_id - 1 : -1;
 
         if (playerId >= 0)
         {
+            // Get the character data and update the character sprite and name
             Characters character = characterData.GetCharacter(playerId);
             characterSpriteStatic.sprite = character.characterSprite;
             characterNameStatic.text = character.characterName;
 
-            // Create the button by readding the image and text
+            // Enable the button by adding image and text
             btn.GetComponent<Image>().enabled = true;
             btn.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "X";
         }
     }
 
+    // Method that is called when the left button is clicked
     public void leftClick()
     {
         ChooseCharacter(slots[0], 1);
     }
 
+    // Method that is called when the right button is clicked
     public void rightClick()
     {
         ChooseCharacter(slots[2], 3);
     }
 
+    // Method that is called when the middle button is clicked
     public void middleClick()
     {
         ChooseCharacter(slots[1], 2);
@@ -97,31 +107,37 @@ public class CanvasSession : MonoBehaviour
 
     public void killLeft()
     {
+         
         Debug.Log("=====>" + slots[0].session_id);
+         // Start a coroutine to call the API and clear the left slot
         StartCoroutine(DoAPI(slots[0].session_id));
     }
 
     public void killMiddle()
     {
+        // Start a coroutine to call the API and clear the middle slot
         StartCoroutine(DoAPI(slots[1].session_id));
     }
 
     public void killRight()
     {
+        // Start a coroutine to call the API and clear the right slot
         StartCoroutine(DoAPI(slots[2].session_id));
     }
     public void ChooseCharacter(Slot index, int slot)
     {
+        // Get the player ID
         int playerId = index.hero_id > 0 ? index.hero_id - 1 : -1;
         Debug.Log("=====>" + playerId);
 
-        if (playerId >= 0)
+        if (playerId >= 0) // If the player ID is valid, modify stats
         {
             Debug.Log("INSIDEEEEE");
             StartCoroutine(ModifyPlayerStats(playerId, index));
         }
         else
         {
+            // If the player ID is invalid, enable the character manager and disable the menu
             session.enabled = false;
             menu.enabled = true;
             characterManager.GetComponent<CharacterManager>().slotID = slot;
@@ -133,8 +149,11 @@ public class CanvasSession : MonoBehaviour
         yield return new WaitForSeconds(1f);
         Characters character = characterData.GetCharacter(playerId);
 
+        // Get the player prefab
+
         GameObject prefab = Resources.Load<GameObject>("Characters/" + character.characterName);
 
+        // Get the player OG prefab
         prefab.GetComponent<PlayerStats>().playerDamage = index.damage;
         prefab.GetComponent<PlayerStats>().playerHealth = index.health;
         prefab.GetComponent<PlayerStats>().playerMana = index.mana;
@@ -150,7 +169,7 @@ public class CanvasSession : MonoBehaviour
         SceneManager.LoadScene(2, LoadSceneMode.Single);
     }
 
-    IEnumerator DoAPI(int index)
+    IEnumerator DoAPI(int index) // index is the session_id
     {
         Debug.Log("=====>" + index);
         yield return new WaitForSeconds(1f);
--- Videogame/QuetzalUnity/Assets/Scripts/Character Menu/Change_Input.cs
+++ Videogame/QuetzalUnity/Assets/Scripts/Character Menu/Change_Input.cs
@@ -6,33 +6,47 @@ using System.Collections.Generic;
 
 public class Change_Input : MonoBehaviour
 {
-    EventSystem system;
-    public Selectable firstInput;
-    public Button submitButton;
+    // Declare variables
+    EventSystem system;             // Used to track currently selected UI element
+    public Selectable firstInput;   // The first input field to select when the scene starts
+    public Button submitButton;     // The submit button
+
     // Start is called before the first frame update
     void Start()
     {
+        // Get reference to the EventSystem
         system = EventSystem.current;
+        
+        // Select the first input field when the scene starts
         firstInput.Select();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check if the "Tab" key is pressed along with the "Left Shift" key
         if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift)){
+            // Get the selectable object above the currently selected object
             Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
             if (next != null){
+                // Select the next object
                 next.Select();
             }
         }
+        // Check if the "Tab" key is pressed
         else if (Input.GetKeyDown(KeyCode.Tab)){
+            // Get the selectable object below the currently selected object
             Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
             if (next != null){
+                // Select the next object
                 next.Select();
             }
         }
+        // Check if the "Return" key is pressed
         else if (Input.GetKeyDown(KeyCode.Return)){
+            // Invoke the "OnClick" event of the submit button
             submitButton.onClick.Invoke();
+            // Print a debug message to the console
             Debug.Log("Submit");
         }
     }
